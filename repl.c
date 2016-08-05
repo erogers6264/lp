ba65699c0a6f7cb502fb694d85de3694a2c22771
Ethan Rogers <erogers6264@gmail.com>
2016-08-04 22:27:18 -0700

    Enable editing with arrow keys at the prompt

--- repl.c
+++ repl.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
+#include <stdlib.h>
 
+#include <editline/readline.h>
+#include <editline/history.h>
 
-/* Declare a buffer for user input size 2048 */
-static char input[2048];
 
 int main(int argc, char** argv) {
 
@@ -12,15 +13,19 @@ int main(int argc, char** argv) {
 
   while (1) {
 
-    /* Output our prompt */
-    fputs("lispy> ", stdout);
+    /* Output our prompt and get user input */
+    char* input = readline("lispy> ");
 
-    /* Read a line of user input of maximum size 2048 */
-    fgets(input, 2048, stdin);
+    // Add input to history
+    add_history(input);
 
     /* Echo input back to user */
-    printf("No you're a %s", input);
+    printf("No you're a %s\n", input);
+
+    // Free retrieved input
+    free(input);
   }
 
   return 0;
 }
+
