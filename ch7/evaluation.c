545df76cc17b8ceb5dcc3b71a2771633edebfcdb
Ethan Rogers <erogers6264@gmail.com>
2017-03-24 10:02:05 -0700

    Create windows readline function

---
+++ ch7/evaluation.c
@@ -0,0 +1,13 @@
+#include "mpc.h"
+
+#ifdef _WIN32
+
+static char buffer[2048];
+
+char* readline(char* prompt) {
+  fputs(prompt, stdout);
+  fgets(buffer, 2048, stdin);
+  char* cpy = malloc(strlen(buffer)+1);
+  cpy[strlen(cpy)-1] = '\0'; // Null termination
+  return cpy;
+}
