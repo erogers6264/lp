80d357647fdad2b64e92935c67a5100d204e66a9
Ethan Rogers <erogers6264@gmail.com>
2017-03-27 14:23:42 -0700

    Modify eval and repl to hand lval instead of long

--- error-handling/error.c
+++ error-handling/error.c
@@ -96,28 +96,23 @@ lval eval_op(lval x, char* op, lval y) {
 }
 
 // + (* 8 8) (* 8 8)
-long eval(mpc_ast_t* t) {
-  /* This function uses recursion to iterate
-     through remaining children*/
-     
-  /* If tagged as number return it directly */
+lval eval(mpc_ast_t* t) {
+
   if (strstr(t->tag, "number")) {
-    return atoi(t->contents);
+    /* Check if there is some error in conversion */
+    errno = 0;
+    long x = strtol(t->contents, NULL, 10);
+    return errno != ERANGE ? lval_num(x) : lval_err(LERR_BAD_NUM);
   }
   
-  /* The operator is always second child. */
   char* op = t->children[1]->contents;
+  lval x = eval(t->children[2]);
   
-  /* We store the third child in `x` */
-  long x = eval(t->children[2]);
-  
-  /* Iterate the remaining children and combine */
   int i = 3;
   while (strstr(t->children[i]->tag, "expr")) {
     x = eval_op(x, op, eval(t->children[i]));
     i++;
   }
-  
   return x;
 }
 
@@ -172,8 +167,8 @@ int main(int argc, char** argv) {
     mpc_result_t r;
     if (mpc_parse("<stdin>", input, Lispy, &r)) {
       
-      long result = eval(r.output);
-      printf("%li\n", result);
+      lval result = eval(r.output);
+      lval_println(result);
       mpc_ast_delete(r.output);
       
     } else {
