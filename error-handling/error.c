cb03aa816683b7f226ac102ca4eaf763b4a32aff
Ethan Rogers <erogers6264@gmail.com>
2017-03-28 09:21:49 -0700

    Modify the grammar to work with s-expressions

--- error-handling/error.c
+++ error-handling/error.c
@@ -135,19 +135,21 @@ lval eval(mpc_ast_t* t) {
 int main(int argc, char** argv) {
   /* Create some parsers */
   mpc_parser_t* Number = mpc_new("number");
-  mpc_parser_t* Operator = mpc_new("operator");
+  mpc_parser_t* Symbol = mpc_new("symbol");
+  mpc_parser_t* Sexpr = mpc_new("sexpr");
   mpc_parser_t* Expr = mpc_new("expr");
   mpc_parser_t* Lispy = mpc_new("lispy");
   
   /* Define them with the following language */
   mpca_lang(MPCA_LANG_DEFAULT,
-    "                                                     \
-      number    : /-?[0-9]+/ ;                            \
-      operator  : '+' | '-' | '*' | '/' ;                 \
-      expr      : <number> | '(' <operator> <expr>+ ')' ; \
-      lispy     : /^/ <operator> <expr>+ /$/ ;            \
+    "                                           \
+      number  : /-?[0-9]+/ ;                    \
+      symbol  : '+' | '-' | '*' | '/' ;         \
+      sexpr   : '(' <expr>* ')' ;               \
+      expr    : <number> | <symbol> | <sexpr> ; \
+      lispy   : /^/ <expr>* /$/ ;               \
     ",
-    Number, Operator, Expr, Lispy);
+    Number, Symbol, Sexpr, Expr, Lispy);
   
   /* Print Version and Exit Information */
   puts("Lispy Version 0.0.0.0.1");
@@ -182,7 +184,7 @@ int main(int argc, char** argv) {
     
   }
   
-  mpc_cleanup(4, Number, Operator, Expr, Lispy);
+  mpc_cleanup(5, Number, Symbol, Sexpr, Expr, Lispy);
 
   return 0;
 }
